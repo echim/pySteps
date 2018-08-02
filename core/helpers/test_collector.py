91ba701b3bcd5b515a9cb2cda01170165b502f73
George Echim <george.echim@gmail.com>
2018-08-02 15:55:57 +0300

    Update test collector.py

--- core/helpers/test_collector.py
+++ core/helpers/test_collector.py
@@ -9,8 +9,20 @@ from core.helpers.os_helpers import get_project_root_path, os
 
 def send_result_to_log_file(test_result):
     run_log_file = os.path.join(get_project_root_path(), 'tests', DefaultSettings.LOG_FILE_NAME.value)
-    with open(run_log_file, mode='a', encoding='utf-8') as prev_content:
-        json.dump(test_result, prev_content, indent=4)
+
+    if not os.path.exists(run_log_file):
+        with open(run_log_file, 'a'):
+            os.utime(run_log_file, None)
+    else:
+        with open(run_log_file) as json_file:
+            try:
+                new_data = json.load(json_file)
+            except Exception:
+                new_data = {'tests': []}
+
+            new_data['tests'].append(test_result)
+            with open(run_log_file, mode='w', encoding='utf-8') as prev_content:
+                json.dump(new_data, prev_content, indent=4, ensure_ascii=False)
 
 
 def send_result_to_endpoint(test_result, endpoint):
