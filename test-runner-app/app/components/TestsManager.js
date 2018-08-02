b5953fe401e8d7a8db100499d91488eea6c506c9
George Echim <george.echim@gmail.com>
2018-08-02 17:12:32 +0300

    Add basic list of test results.

--- test-runner-app/app/components/TestsManager.js
+++ test-runner-app/app/components/TestsManager.js
@@ -1,9 +1,11 @@
 // @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import styles from './TestsManager.css';
+import './TestsManager.css';
 import { Redirect } from 'react-router';
 import { isNull } from 'util';
+var fs = require('fs');
+var path = require('path')
 
 type Props = {};
 
@@ -13,7 +15,10 @@ export default class TestsManager extends Component<Props> {
     super(props);
     this.state = {
       testsPath: localStorage.getItem('tests_path') || null,
+      testResults: null
     }
+
+    this.loadTestResults()
   }
 
   render() {
@@ -24,10 +29,52 @@ export default class TestsManager extends Component<Props> {
     return (
       <div>
         <button onClick={this.resetTestsPath}>Clear tests path</button>
-        <Link to="/home">to Home</Link>
+        <button onClick={this.reloadPage}>Reload</button>
         <p>Tests path: {this.state.testsPath}</p>
+
+        <div>
+          {
+            this.state.testResults && this.state.testResults.tests ?
+              this.state.testResults.tests.map((test, index) => {
+                const options = { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+                const date = new Date(test.timestamp * 1000)
+                return (
+                  <div key={index}>
+                    <p>-------------------------------------------------------------------------------------------------</p>
+                    <p>Name: {test.test}</p>
+                    <p className={test.status} >Status: {test.status}</p>
+                    <p>Duration: {test.duration} seconds</p>
+                    <p>Run Date: {date.toLocaleDateString('en-US', options)}</p>
+                  </div>
+                )
+              }) : null
+          }
+        </div>
+
       </div>
     )
+  }
+
+
+  loadTestResults = () => {
+    if (isNull(this.state.testsPath)) {
+      return
+    }
+    const filePath = path.join(this.state.testsPath, 'pyTest_runs.json')
+
+    fs.readFile(filePath, 'utf8', (err, testResults) => {
+      if (err) {
+        alert('Unable to load test results file.')
+        throw new Error('Unable to load test results file.')
+      }
+      this.setState({
+        testResults: JSON.parse(testResults)
+      });
+    });
+  }
+
+  reloadPage = () => {
+    window.location.reload()
   }
 
   resetTestsPath = () => {
